c998f5bf1686e7a5cb802a6aff91ba3513a648ff
killingvoice <kyung2687@gmail.com>
2022-01-20 14:55:26 +0000

    Frame Home page

---
+++ html/home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<?php
+	//세션 확인 후 메인 화면으로 이동
+?>
+
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="../stylesheet/main.css" />
+		<link rel="stylesheet" href="../stylesheet/home.css" />
+	</head>
+	<body>
+		<div class="top-div"></div>
+		<div class="flex1 main-div flex-center flex-col">
+			<div class="flex post-div flex-col">
+				<div class="test1 user-div"></div>
+				<div class="test2 image-div"></div>
+				<div class="test3 button-div"></div>
+				<div class="test1 like-div"></div>
+				<div class="test2 text-div"></div>
+				<div class="test3 date-div"></div>
+			</div>
+		</div>
+	</body>
+</html>
