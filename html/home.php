e968fc06f45b6d0ff03f11650f31b0cd3aa50ad4
killingvoice <kyung2687@gmail.com>
2022-01-23 13:48:09 +0000

    feed card dev

--- html/home.php
+++ html/home.php
@@ -14,10 +14,25 @@
 		<div class="top-div"></div>
 		<div class="flex1 main-div flex-center flex-col">
 			<div class="flex post-div flex-col">
-				<div class="test1 user-div"></div>
-				<div class="test2 image-div"></div>
-				<div class="test3 button-div"></div>
-				<div class="test1 like-div"></div>
+				<div class="user-div flex flex-center">
+					<div class="test1 profile"></div>
+					<div class="flex1 flex-col">
+						<a class="user">asdf</a>
+						<a class="location">asdf</a>
+					</div>
+					<div class="icon option"></div>
+				</div>
+				<div style="background-image: url('/php/clone_insta/image/20220118135206.jpg');" class="image-div"></div>
+				<div class="button-div flex flex-center">
+					<div class="icon unlike"></div>
+					<div class="icon speech"></div>
+					<div class="icon direct"></div>
+					<div class="flex1"></div>
+					<div class="icon mark"></div>
+				</div>
+				<div class="test1 like-div">
+					<span></span><a>좋아합니다.</a>
+				</div>
 				<div class="test2 text-div"></div>
 				<div class="test3 date-div"></div>
 			</div>
